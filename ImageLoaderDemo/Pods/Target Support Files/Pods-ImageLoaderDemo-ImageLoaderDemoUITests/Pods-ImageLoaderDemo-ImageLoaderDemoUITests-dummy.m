0fc92e0917b024831a6b83eb9a5786069a6872f7
Jeni <jenikhant14@gmail.com>
2020-01-04 20:14:07 +0530

    Initialized with pod and added alamofire for api call

---
+++ ImageLoaderDemo/Pods/Target Support Files/Pods-ImageLoaderDemo-ImageLoaderDemoUITests/Pods-ImageLoaderDemo-ImageLoaderDemoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ImageLoaderDemo_ImageLoaderDemoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ImageLoaderDemo_ImageLoaderDemoUITests
+@end
