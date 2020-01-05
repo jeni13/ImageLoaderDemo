991a080f4f1f9182450a3df1677e676b056dd1cd
Jeni <jenikhant14@gmail.com>
2020-01-05 08:23:33 +0530

    Added model and code for api call

--- ImageLoaderDemo/ImageLoaderDemo/Network/ImageLoaderNetworkRequest.swift
+++ ImageLoaderDemo/ImageLoaderDemo/Network/ImageLoaderNetworkRequest.swift
@@ -7,8 +7,27 @@
 //
 
 import Foundation
+import Alamofire
 
 // MARK: - ImageLoaderNetworkRequest
 class ImageLoaderNetworkRequest {
+   
+    let url = "https://api.unsplash.com/photos"
+    let headers = ["Authorization": "Client-ID 937634a53ef59a1151f7da9d2ea6d0dd5b24fca2aa47513f9d03aa90cc9fcdfb"]
     
+    func getDataFromUnSplash(param: [String: Any?]?, completion: @escaping ([ImageLoaderModel]) -> Void) {
+        Alamofire.request(url, method: .get, parameters: nil, headers: headers).responseJSON { (response) in
+            if let value = response.value {
+                let decoder = JSONDecoder()
+                do {
+                    let jsonData = try JSONSerialization.data(withJSONObject: value, options: .prettyPrinted)
+                    let data = try decoder.decode([ImageLoaderModel].self, from: jsonData)
+                    completion(data)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+            }
+    }
 }
+
