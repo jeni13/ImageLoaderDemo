fe563c3a538a54fa145a845db66667ae730502dc
Jeni <jenikhant14@gmail.com>
2020-01-05 10:33:20 +0530

    Added large image view on click of cell item Functionality to show and hide large image

--- ImageLoaderDemo/ImageLoaderDemo/ViewController/ImageLoaderViewController.swift
+++ ImageLoaderDemo/ImageLoaderDemo/ViewController/ImageLoaderViewController.swift
@@ -12,6 +12,9 @@ import UIKit
 class ImageLoaderViewController: UIViewController {
    
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var containerView: UIVisualEffectView!
+    @IBOutlet weak var largeImageView: UIImageView!
+
     let cellIdentifier = "ImageLoaderCollectionViewCell"
     private let presenter = ImageLoaderPresenter()
     
@@ -26,6 +29,10 @@ class ImageLoaderViewController: UIViewController {
     private func registerNib() {
         self.collectionView.register(UINib(nibName: cellIdentifier, bundle: nil),
                                      forCellWithReuseIdentifier: cellIdentifier)
+    }
+    @IBAction func closeClicked(_ sender: UIButton) {
+        self.largeImageView.image = nil
+        self.containerView.isHidden = true
     }
 }
 
@@ -43,8 +50,14 @@ extension ImageLoaderViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! ImageLoaderCollectionViewCell
-        presenter.getImageForIndex(indexPath.row) { (data) in
-            cell.downloadedImageView.image = UIImage(data: data)
+        DispatchQueue.global().async {
+            self.presenter.getImageForIndex(indexPath.row) { (data) in
+                DispatchQueue.main.async {
+                    cell.downloadedImageView.image = UIImage(data: data)
+                    //                cell.layoutIfNeeded()
+                    collectionView.layoutIfNeeded()
+                }
+            }
         }
         return cell
     }
@@ -53,5 +66,20 @@ extension ImageLoaderViewController: UICollectionViewDataSource {
 }
 // MARK: - Collection View delegate
 extension ImageLoaderViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let cell = collectionView.cellForItem(at: indexPath) as? ImageLoaderCollectionViewCell {
+            self.containerView.isHidden = false
+            self.largeImageView.image = cell.downloadedImageView.image
+        }
+    }
+}
+
+extension ImageLoaderViewController: UICollectionViewDelegateFlowLayout {
+ 
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      sizeForItemAt indexPath: IndexPath) -> CGSize {
     
+    return CGSize(width: self.view.frame.width, height: self.view.frame.width)
+  }
 }
