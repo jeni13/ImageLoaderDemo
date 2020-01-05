4de77730fce9132f7192c639deaec5cc5e88b608
Jeni <jenikhant14@gmail.com>
2020-01-05 10:33:26 +0530

    Added method for removing cached data

--- ImageLoaderDemo/ImageLoaderSDK/ImageLoaderSDK.swift
+++ ImageLoaderDemo/ImageLoaderSDK/ImageLoaderSDK.swift
@@ -13,9 +13,28 @@ public class ImageLoaderSDK {
     
     public static var instance = ImageLoaderSDK()
     
-    public func getImageWith(_ urlString: String, completion: (Data) -> Void) {
-        if let url = URL(string: urlString), let data = try? Data(contentsOf: url) {
-            completion(data)
+    // MARK: - Check if image data is cached or get the image data
+    public func getImageWith(_ urlString: String, completion: @escaping (Data) -> Void) {
+        if let cachedData = UserDefaults.standard.data(forKey: urlString) {
+            completion(cachedData)
+        } else {
+            if let url = URL(string: urlString), let data = try? Data(contentsOf: url) {
+                UserDefaults.standard.set(data, forKey: urlString)
+                completion(data)
+            }
         }
+    }
+    
+    // MARK: - For removing single cached data (For future scope)
+    public func removeCachedImageFor(imageString: String) {
+        UserDefaults.standard.removeObject(forKey: imageString)
+    }
+    
+    // MARK: - For Removing all cached images once we dont required
+    public func removeAllCachedImages() {
+        let domain = Bundle.main.bundleIdentifier!
+        UserDefaults.standard.removePersistentDomain(forName: domain)
+        UserDefaults.standard.synchronize()
+        
     }
 }
